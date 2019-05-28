5d914f6d7c3f80d51177bf40c98b199ce309efbb
vinicius <viniciusfon96@gmail.com>
2019-05-28 16:28:27 -0300

    adição da criação de usuario, token jwt e recuperacao de senha

---
+++ start/routes.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store' )
+Route.post('sessions', 'SessionController.store')
+Route.post('passwords','ForgotPasswordController.store')
